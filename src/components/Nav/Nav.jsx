5d2de4bd64a0d182c7c51ed3d9fd2af22eeb475b
kalicialopez <104682798+kalicialopez@users.noreply.github.com>
2023-02-21 16:33:21 +0800

    Obtaining data from API

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
+
 function Nav() {
   return (
     <nav>
       <Link to="/">Home</Link>
-      <Link to="/project">Project</Link>
     </nav>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,27 @@
+import { useState, useEffect } from "react";
+
 // Components
 import ProjectCard from "../components/ProjectCard/ProjectCard";
 
-// Data
-import { allProjects } from "../data";
-
 function HomePage() {
+    //State
+    const [projectList, setProjectList] = useState([]);
+    
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_API_URL}projects`)
+        .then((results) => {
+            return results.json();
+        })
+        .then((data) => {
+            setProjectList(data);
+        });
+    }, []);
+
+
+    // Some changes here that were not in thinkific regarding projectData
   return (
     <div id="project-list">
-      {allProjects.map((project, key) => {
+      {projectList.map((project, key) => {
         return <ProjectCard key={key} projectData={project} />;
       })}
     </div>
