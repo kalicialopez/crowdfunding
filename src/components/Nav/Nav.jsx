a1f18a6fef525b158caeab9beb22772dce273ed0
kalicialopez <104682798+kalicialopez@users.noreply.github.com>
2023-03-22 23:11:10 +0800

    attempt at hamburger nav unsuccessful

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+
+// Styles
 import "./Nav.css";
 
 function Nav(props) {
@@ -59,15 +61,106 @@ function Nav(props) {
           )}
         </div>
       </ul>
-
-      <div className="burger">
-        <div className="line1"></div>
-        <div className="line2"></div>
-        <div className="line3"></div>
-      </div>
-      {/* </div> */}
     </nav>
   );
 }
 
 export default Nav;
+
+// Attempt 2 at hamburger-navbar.
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+
+// // Styles
+// import "./Nav.css";
+
+// // Icons
+// import { MdClose } from "react-icons/md";
+// import { FiMenu } from "react-icons/fi";
+
+// function Nav(props) {
+//   const [navbarOpen, setNavbarOpen] = useState(false);
+
+//   const { loggedIn, setLoggedIn } = props;
+// }
+
+// const handleClick = () => {
+//   window.localStorage.removeItem("token");
+//   setLoggedIn(false);
+// };
+
+// const handleToggle = () => {
+//   setNavbarOpen(!navbarOpen);
+// };
+
+// const closeMenu = () => {
+//   setNavbarOpen(false);
+// };
+
+// return (
+//   <>
+//     <nav>
+//       <div id="logo-container">
+//         <Link to="/">
+//           <img src={`/assets/media/logo.png`} id="logo" alt="EducAid-logo" />
+//         </Link>
+//       </div>
+
+//       <button onClick={handleToggle}>
+//         {navbarOpen ? (
+//           <MdClose style={{ color: "black", width: "30px", height: "30px" }} />
+//         ) : (
+//           <FiMenu style={{ color: "black", width: "30px", height: "30px" }} />
+//         )}
+//       </button>
+
+//       <ul className={`navbar ${navbarOpen ? " showMenu" : ""}`}>
+//         <li className="nav-link" onClick={() => closeMenu()}>
+//           <Link to="/">Home</Link>
+//         </li>
+//         <li className="nav-link" onClick={() => closeMenu()}>
+//           <Link to="/about">About</Link>
+//         </li>
+//         <li className="nav-link" onClick={() => closeMenu()}>
+//           <Link to="/all-projects">Campaigns</Link>
+//         </li>
+//         <li className="nav-link" onClick={() => closeMenu()}>
+//           <Link to="/create-project">Start a Campaign</Link>
+//         </li>
+//         <li className="nav-link" onClick={() => closeMenu()}>
+//           <Link to="/contact">Contact</Link>
+//         </li>
+
+//         {loggedIn && (
+//           <li className="nav-link" onClick={() => closeMenu()}>
+//             <Link to="/user/session">My EducAid</Link>
+//           </li>
+//         )}
+
+//         {!loggedIn && (
+//           <li className="nav-link" onClick={() => closeMenu()}>
+//             <Link to="/register">Sign Up</Link>
+//           </li>
+//         )}
+//         {!loggedIn && (
+//           <li className="nav-link" onClick={() => closeMenu()}>
+//             <Link to="/login">Login</Link>
+//           </li>
+//         )}
+
+//         <div id="logout-wrapper">
+//           {loggedIn && (
+//             <li className="nav-link" onClick={() => closeMenu()}>
+//               <button id="logout-button" onClick={handleClick}>
+//                 Log Out
+//               </button>
+//             </li>
+//           )}
+//         </div>
+//       </ul>
+//     </nav>
+//   </>
+// );
+// // }
+
+// export default Nav;
