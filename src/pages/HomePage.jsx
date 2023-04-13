65f49861ecdcba121e8a309a130d3494b23f8983
kalicialopez <104682798+kalicialopez@users.noreply.github.com>
2023-04-13 16:32:17 +0800

    Added border boxes to sections on homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -41,42 +41,45 @@ function HomePage() {
   return (
     <>
       <body className="page-body">
-        <h1>Welcome to EducAid,</h1>
-        <h2>
-          The non-for-profit crowdfunding platform that empowers individuals to
-          pursue their dreams of education without financial barriers.
-        </h2>
-        <div className="page-image-container">
-          <img
-            src={`/assets/media/financial-aid.jpg`}
-            className="page-image"
-            alt="financial-aid"
-          />
+        <h1>Welcome to EducAid</h1>
+        <div className="homepage-intro-text-container">
+          <h2>
+            The non-for-profit crowdfunding platform that empowers individuals
+            to pursue their dreams of education without financial barriers.
+          </h2>
+          <div className="page-image-container">
+            <img
+              src={`/assets/media/financial-aid.jpg`}
+              className="page-image"
+              alt="financial-aid"
+            />
+          </div>
+          <p className="page-text">
+            Education is powerful, and when accessed by those with the right
+            attitude and perseverance, has the capacity to change lives. Whether
+            you are a stay-at-home mother, 20 years into your existing career,
+            have never had a 'career' per se, or you're seeking change, yet find
+            yourself hindered by financial constraints, EducAid is here to help.{" "}
+            <br />
+            <br />
+            Similarly, if you understand the impact education can have on the
+            life trajectory of an individual, and are in the fortunate position
+            to be able to help someone achieve their career aspirations, EducAid
+            can assist with this too.
+          </p>
         </div>
-        <p className="page-text">
-          Education is powerful, and when accessed by those with the right
-          attitude and perseverance, has the capacity to change lives. Whether
-          you are a stay-at-home mother, 20 years into your existing career,
-          have never had a 'career' per se, or you're seeking change, yet find
-          yourself hindered by financial constraints, EducAid is here to help.{" "}
-          <br />
-          <br />
-          Similarly, if you understand the impact education can have on the life
-          trajectory of an individual, and are in the fortunate position to be
-          able to help someone achieve their career aspirations, EducAid can
-          assist with this too.
-        </p>
 
         <h2>Featured Active Campaigns </h2>
+        <div className="homepage-projects-container">
+          <button onClick={shuffleProjectList} className="button">
+            Shuffle
+          </button>
 
-        <button onClick={shuffleProjectList} className="button">
-          Shuffle
-        </button>
-
-        <div id="project-list">
-          {shuffledSortedProjectList.map((project, key) => {
-            return <ProjectCard key={key} projectData={project} />;
-          })}
+          <div id="project-list">
+            {shuffledSortedProjectList.map((project, key) => {
+              return <ProjectCard key={key} projectData={project} />;
+            })}
+          </div>
         </div>
 
         {/* <div id="project-list">
